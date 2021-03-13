d04f8d241823c8a6a9479c3e96ea061c5a1636cb
hg-hg <46097315+hg-hg@users.noreply.github.com>
2021-03-13 21:14:24 +0800

    isNaN

--- js/analyze.js
+++ js/analyze.js
@@ -45,7 +45,7 @@ function update_analyze_result(result){
     var num_display = Math.min(init_num_display, result_len, init_upper_bound);
     stopwords = ['i','me','my','myself','we','our','ours','ourselves','you','your','yours','yourself','yourselves','he','him','his','himself','she','her','hers','herself','it','its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that','these','those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only','own','same','so','than','too','very','s','t','can','will','just','don','should','now'];
     for (var i = 0; i < num_display; i++) {
-        if (!stopwords.includes(result[i][0].toLowerCase())) {
+        if (isNaN(result[i][0]) && isNaN(parseFloat(result[i][0])) && !stopwords.includes(result[i][0].toLowerCase())) {
             keywords += convert_word_to_link(result[i][0]);
             frequency += convert_word_to_element(result[i][1]);
         }else{
