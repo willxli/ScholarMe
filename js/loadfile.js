79ff4373265019500e581d700b9fbc2468547a45
Lvweikun <Lvweikun@bytedance.com>
2021-03-22 17:22:23 +0800

    add upload button

---
+++ js/loadfile.js
@@ -0,0 +1,19 @@
+document.getElementById("uploadButton").onclick = function() {
+    console.log("Uploading...")
+    var file = document.getElementById("uploadButton").files[0];
+    var extension = file.name.substring(file.name.lastIndexOf('.')); console.log(extension);
+    var validFileType = ".txt";
+    if (validFileType.toLowerCase().indexOf(extension) < 0) {
+        alert("please select valid file type. The supported file types are .txt");
+        return false;
+    } else {
+        var fileReader = new FileReader();
+        fileReader.onload = function (e) {
+            var text = e.target.result;
+            document.getElementById("text").value = text;
+            var x = document.getElementById("text");
+        }
+        fileReader.readAsText(file, "UTF-8");
+}
+
+}
