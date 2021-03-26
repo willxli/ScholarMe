b3d0b129cde85131df7ad530fb24caaff2ba7975
Lvweikun <Lvweikun@bytedance.com>
2021-03-26 22:22:57 +0800

    have some error on style

--- js/loadfile.js
+++ js/loadfile.js
@@ -6,6 +6,7 @@ document.getElementById("uploadInput").onchange = function() {
     console.log("Uploading...")
     var file = this.files[0];
     var extension = file.name.substring(file.name.lastIndexOf('.'));
+    console.log(file.name)
     console.log(extension);
     if (extension !== ".txt") {
         alert("please select valid file type. The supported file types are .txt");
@@ -18,5 +19,6 @@ document.getElementById("uploadInput").onchange = function() {
         }
         fileReader.readAsText(file, "UTF-8");
     }
+    document.getElementById("upload").innerHTML += file.name;
     return true;
 }
